389e2825bd7001efa8056185b929c8c86246d03d
Prz-Ellam <PerezAlex088@outlook.com>
2023-09-09 21:45:46 -0600

    feat: migrations

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,8 +2,7 @@ import app from '@config/app';
 import { logger } from '@config/logger';
 import { databaseConnection } from '@config/connection';
 
-void databaseConnection();
-
-app.listen(app.get('port'), () => {
+app.listen(app.get('port'), async () => {
+    await databaseConnection();
     logger.info(`Server started on port ${app.get('port')}`);
 });
