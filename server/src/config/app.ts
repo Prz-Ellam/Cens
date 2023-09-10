80e6866b8c7c89c1f82fcf1c5a94fa7a93aaf753
Prz-Ellam <PerezAlex088@outlook.com>
2023-09-09 21:20:18 -0600

    fix: user endpoints

--- server/src/config/app.ts
+++ server/src/config/app.ts
@@ -4,6 +4,8 @@ import { env } from '@config/env';
 import morganMiddleware from '@config/morgan';
 import { trim_all } from 'request_trimmer';
 import { notFoundMiddleware } from '@/middlewares/not-found.middleware';
+import userRouter from '@/routes/user.routes';
+import authRouter from '@/routes/auth.routes';
 
 const app = express();
 app.set('port', env.get('server.port'));
@@ -14,6 +16,9 @@ app.use(helmet());
 app.use(trim_all);
 
 app.disable('x-powered-by');
+
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', userRouter);
 
 app.all('*', notFoundMiddleware);
 
