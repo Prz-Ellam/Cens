a7405bb61fe8c48bdf6d5d49c78d993709c1c799
Prz-Ellam <PerezAlex088@outlook.com>
2023-09-09 21:03:24 -0600

    feat: middlewares

--- server/src/config/app.ts
+++ server/src/config/app.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import helmet from 'helmet';
 import { env } from '@config/env';
 import morganMiddleware from '@config/morgan';
+import { trim_all } from 'request_trimmer';
+import { notFoundMiddleware } from '@/middlewares/not-found.middleware';
 
 const app = express();
 app.set('port', env.get('server.port'));
@@ -9,7 +11,10 @@ app.set('port', env.get('server.port'));
 app.use(express.json({ limit: '1kb' }));
 app.use(morganMiddleware);
 app.use(helmet());
+app.use(trim_all);
 
 app.disable('x-powered-by');
+
+app.all('*', notFoundMiddleware);
 
 export default app;
