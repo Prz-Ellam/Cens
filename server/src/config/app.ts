505655dd0a214734db3a7f66258f69daaef25de5
Prz-Ellam <PerezAlex088@outlook.com>
2023-09-09 20:50:43 -0600

    feat: morgan middleware

--- server/src/config/app.ts
+++ server/src/config/app.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import helmet from 'helmet';
 import { env } from '@config/env';
+import morganMiddleware from '@config/morgan';
 
 const app = express();
 app.set('port', env.get('server.port'));
 
 app.use(express.json({ limit: '1kb' }));
+app.use(morganMiddleware);
 app.use(helmet());
 
 app.disable('x-powered-by');
