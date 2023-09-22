c8b1db30a8956c6d70e74f13aac8b8d52caee334
Prz-Ellam <PerezAlex088@outlook.com>
2023-09-21 23:31:38 -0600

    feat: prettier

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,11 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 
 const $root = document.getElementById('root');
 ReactDOM.createRoot($root).render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
-)
+);
