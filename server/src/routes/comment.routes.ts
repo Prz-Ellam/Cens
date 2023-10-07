a6f5ba7f20a592629681441d5f2a57f65c206c4d
Prz-Ellam <PerezAlex088@outlook.com>
2023-10-06 22:41:17 -0600

    One poll

--- server/src/routes/comment.routes.ts
+++ server/src/routes/comment.routes.ts
@@ -5,6 +5,7 @@ import { Router } from 'express';
 const commentRouter = Router();
 
 commentRouter.post('/');
+commentRouter.get('/:commentId', authMiddleware, CommentController.findOne);
 commentRouter.put('/:commentId', authMiddleware, CommentController.update);
 commentRouter.delete('/:commentId', authMiddleware, CommentController.delete);
 
