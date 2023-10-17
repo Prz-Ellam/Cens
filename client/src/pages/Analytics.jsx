b1250cde1ce5df2493ffc9ddd50ffef1af354d67
Prz-Ellam <PerezAlex088@outlook.com>
2023-10-17 00:15:14 -0600

    Page ideas

--- client/src/pages/Analytics.jsx
+++ client/src/pages/Analytics.jsx
@@ -1,7 +1,79 @@
+// import React from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top'
+    },
+    title: {
+      display: true,
+      text: 'Por demografica'
+    }
+  }
+};
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: labels.map(() => Math.random() * 1000),
+      backgroundColor: 'rgba(255, 99, 132, 0.5)'
+    },
+    {
+      label: 'Dataset 2',
+      data: labels.map(() => Math.random() * 1000),
+      backgroundColor: 'rgba(53, 162, 235, 0.5)'
+    }
+  ]
+};
+
 function Analytics() {
-    return (
-        <></>
-    )
+  return (
+    <section className="h-full overflow-auto">
+      <div className="w-4/5 mx-auto my-4">
+        <h1 className="text-4xl text-center font-semibold mb-4 text-gray-300">
+          Analiticas
+        </h1>
+        <div className="grid grid-cols-2 grid-rows-2 gap-4 p-4">
+      <div className="bg-white rounded shadow-md p-4">
+        <Bar data={data} options={options} />
+      </div>
+      <div className="bg-white rounded shadow-md p-4">
+        <Bar data={data} options={options} />
+      </div>
+      <div className="bg-white rounded shadow-md p-4">
+        <Bar data={data} options={options} />
+      </div>
+      <div className="bg-white rounded shadow-md p-4">
+        <Bar data={data} options={options} />
+      </div>
+    </div>
+      </div>
+    </section>
+  );
 }
 
 export default Analytics;
--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,7 +1,49 @@
+import SurveyForm from '@/components/SurveyForm';
+import { useEffect, useState } from 'react';
+import { useAuth } from '../hooks/useAuth';
+import axios from '@/services/api';
+
 function Search() {
-    return (
-        <></>
-    )
+  const { user } = useAuth();
+
+  const [polls, setPolls] = useState([]);
+
+  const fetchPolls = async () => {
+    try {
+      const response = await axios(`/users/${user.id}/polls/following`);
+
+      setPolls(response.data);
+    } catch (error) {
+      console.error('Error fetching polls:', error);
+    }
+  };
+
+  useEffect(() => {
+    if (user) {
+      fetchPolls();
+    }
+  }, []);
+
+  return (
+    <section className="h-full overflow-auto">
+      <div className="w-4/5 mx-auto my-4">
+        <h1 className="text-4xl text-center font-semibold mb-4 text-gray-300">
+          Busqueda
+        </h1>
+        <section className="flex flex-col gap-4 rounded-lg overflow-y-scroll">
+          {polls &&
+            polls.map((poll, index) => (
+              <SurveyForm
+                key={index}
+                poll={poll}
+                onDelete={async () => {}}
+                onUpdate={async () => {}}
+              />
+            ))}
+        </section>
+      </div>
+    </section>
+  );
 }
 
 export default Search;
